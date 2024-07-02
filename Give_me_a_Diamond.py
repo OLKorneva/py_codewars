4a3c27ade7c2de529e7328d3d28e1e2761e372df
Olesya <fenixa@inbox.ru>
2024-07-02 18:53:21 +0300

    feat: add Give_me_a_Diamond

---
+++ Give_me_a_Diamond.py
@@ -0,0 +1,11 @@
+def diamond(n):
+    if n > 0 and n % 2 == 1:
+        res = []
+        for i in range(1, n - 1, 2):
+            num_space = (n - i) // 2
+            res.append(' ' * num_space + '*' * i)
+        res = res + ['*' * n] + res[::-1]
+        return '\n'.join(res) + '\n'
+
+
+print(diamond(3))
