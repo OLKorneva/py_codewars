509adf3b67423ae94acd726914446044bc1bc2f6
Olesya <fenixa@inbox.ru>
2024-07-18 11:16:02 +0300

    feat: add Product_of_consecutive_Fibnumbers

---
+++ Product_of_consecutive_Fibnumbers.py
@@ -0,0 +1,7 @@
+#task: https://www.codewars.com/kata/5541f58a944b85ce6d00006a/train/python
+
+def product_fib(_prod):
+    a, b = 0, 1
+    while a * b < _prod:
+        a, b = b, a + b
+    return [a, b, a * b == _prod]
